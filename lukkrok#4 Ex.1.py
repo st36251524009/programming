18aa97e9373286b5c435d2d034184075103ffdd4
st36251524009 <52314794+st36251524009@users.noreply.github.com>
2019-07-05 14:04:04 +0700

    Add files via upload

---
+++ lukkrok#4 Ex.1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("BEAR SHOP")
+print('''--------------------------------''')
+print('''ice     x 8 THB''')
+print("banan x 1  12 THB")      
+print("MAMA x  14 THB")
+print('''--------------------------------''')
+print("Total  34 THB")
+      
+
+
+# In[8]:
+
+
+print(type(10))
+
+
+# In[6]:
+
+
+print(type(10))
+print(type("Hello"))
+print(type(True))
+print(type(10.3))
+print(type(1j))
+
+
+# In[10]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
